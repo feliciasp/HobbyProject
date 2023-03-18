f034f35d34a44cbbc7acef1f4bb59e5ec3157b57
Felicia Spångberg <feliciaspangberg@gmail.com>
2023-03-18 14:12:59 +0100

    Adding our engine to source

---
+++ Source/HobbyProject/HobbyProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HobbyProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HobbyProject, "HobbyProject" );
