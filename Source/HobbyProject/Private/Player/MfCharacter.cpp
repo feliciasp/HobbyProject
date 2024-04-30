4633fbeaffea74a1c1ddd295d9155892a822d316
Felicia Spångberg <feliciaspangberg@gmail.com>
2024-05-01 00:36:45 +0200

    Added custom character & basic inputs
    - Added MfCharacter class with child BP
    - Added base for some camera
    - Added bare minimum for moving character

---
+++ Source/HobbyProject/Private/Player/MfCharacter.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/MfCharacter.h"
+
+//comp includes
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AMfCharacter::AMfCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Initialize comps 
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+// Called when the game starts or when spawned
+void AMfCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMfCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMfCharacter::MoveForward(float InMovement)
+{
+	AddMovementInput(GetActorForwardVector(), InMovement);
+}
+
+// Called to bind functionality to input
+void AMfCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//Basic axis movement
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMfCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+
+}
+
---
+++ Source/HobbyProject/Public/Player/MfCharacter.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MfCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class HOBBYPROJECT_API AMfCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMfCharacter();
+
+protected:
+
+	//Start 3D camera
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+	//End 3D camera
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Start movement
+	void MoveForward(float InMovement);
+	//End movement
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
